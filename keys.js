9cb28e5565f18eb72dbbd7058a7a948610a1e337
Marv_Tron <marvinatryonjr@gmail.com>
2019-02-28 15:24:04 -0800

    a lot happened here

---
+++ keys.js
@@ -0,0 +1,6 @@
+/*LIRI_Bot
+Language Interpretation and Recognition Interface: Command line Node.js app that searches for songs, performances, and films using the Spotify, BandsInTown, and OMDB APIs. https://marvtron.github.io/My-Portfolio/
+
+Spotify: 
+Client ID 8d40010e05104beb92a3216118cf9e4a
+Client Secret a758b7d3615747e4bfc80c0cedaaab98 */
